d243e33b3b1c1d5e2520edf659c2e8b8526fb710
eduardosalaz <eduardosalaz@outlook.com>
2021-04-20 13:32:36 -0500

    lo mas tedioso de la vida

--- src/reloj/RelojGUI.java
+++ src/reloj/RelojGUI.java
@@ -74,8 +74,9 @@ public class RelojGUI {
                       }
 
                       lblReloj.setText(""+hour + ":" + min + " " + AMoPM);
+                      gradosHora(hour, am_pm);
+                      gradosMinutos(min);
                       sleep(1000);
-
                   }
               }catch (Exception ex){
                   ex.printStackTrace();
@@ -84,5 +85,284 @@ public class RelojGUI {
         };
         th.start();
     }
+
+    private void gradosMinutos(int min) {
+        switch (min){
+            case 0:
+                //serial send(xxgrados);
+                break;
+            case 1:
+                //serial send(xxgrados);
+                break;
+            case 2:
+                //serial send(xxgrados);
+                break;
+            case 3:
+                //serial send(xxgrados);
+                break;
+            case 4:
+                //serial send(xxgrados);
+                break;
+            case 5:
+                //serial send(xxgrados);
+                break;
+            case 6:
+                //serial send(xxgrados);
+                break;
+            case 7:
+                //serial send(xxgrados);
+                break;
+            case 8:
+                //serial send(xxgrados);
+                break;
+            case 9:
+                //serial send(xxgrados);
+                break;
+            case 10:
+                //serial send(xxgrados);
+                break;
+            case 11:
+                //serial send(xxgrados);
+                break;
+            case 12:
+                //serial send(xxgrados);
+                break;
+            case 13:
+                //serial send(xxgrados);
+                break;
+            case 14:
+                //serial send(xxgrados);
+                break;
+            case 15:
+                //serial send(xxgrados);
+                break;
+            case 16:
+                //serial send(xxgrados);
+                break;
+            case 17:
+                //serial send(xxgrados);
+                break;
+            case 18:
+                //serial send(xxgrados);
+                break;
+            case 19:
+                //serial send(xxgrados);
+                break;
+            case 20:
+                //serial send(xxgrados);
+                break;
+            case 21:
+                //serial send(xxgrados);
+                break;
+            case 22:
+                //serial send(xxgrados);
+                break;
+            case 23:
+                //serial send(xxgrados);
+                break;
+            case 24:
+                //serial send(xxgrados);
+                break;
+            case 25:
+                //serial send(xxgrados);
+                break;
+            case 26:
+                //serial send(xxgrados);
+                break;
+            case 27:
+                //serial send(xxgrados);
+                break;
+            case 28:
+                //serial send(xxgrados);
+                break;
+            case 29:
+                //serial send(xxgrados);
+                break;
+            case 30:
+                //serial send(xxgrados);
+                break;
+            case 31:
+                //serial send(xxgrados);
+                break;
+            case 32:
+                //serial send(xxgrados);
+                break;
+            case 33:
+                //serial send(xxgrados);
+                break;
+            case 34:
+                //serial send(xxgrados);
+                break;
+            case 35:
+                //serial send(xxgrados);
+                break;
+            case 36:
+                //serial send(xxgrados);
+                break;
+            case 37:
+                //serial send(xxgrados);
+                break;
+            case 38:
+                //serial send(xxgrados);
+                break;
+            case 39:
+                //serial send(xxgrados);
+                break;
+            case 40:
+                //serial send(xxgrados);
+                break;
+            case 41:
+                //serial send(xxgrados);
+                break;
+            case 42:
+                //serial send(xxgrados);
+                break;
+            case 43:
+                //serial send(xxgrados);
+                break;
+            case 44:
+                //serial send(xxgrados);
+                break;
+            case 45:
+                //serial send(xxgrados);
+                break;
+            case 46:
+                //serial send(xxgrados);
+                break;
+            case 47:
+                //serial send(xxgrados);
+                break;
+            case 48:
+                //serial send(xxgrados);
+                break;
+            case 49:
+                //serial send(xxgrados);
+                break;
+            case 50:
+                //serial send(xxgrados);
+                break;
+            case 51:
+                //serial send(xxgrados);
+                break;
+            case 52:
+                //serial send(xxgrados);
+                break;
+            case 53:
+                //serial send(xxgrados);
+                break;
+            case 54:
+                //serial send(xxgrados);
+                break;
+            case 55:
+                //serial send(xxgrados);
+                break;
+            case 56:
+                //serial send(xxgrados);
+                break;
+            case 57:
+                //serial send(xxgrados);
+                break;
+            case 58:
+                //serial send(xxgrados);
+                break;
+            case 59:
+                //serial send(xxgrados);
+                break;
+        }
+    }
+
+    private void gradosHora(int hour, int am_pm){
+        switch(hour){
+            case 1:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 2:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 3:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 4:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 5:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 6:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 7:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 8:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 9:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 10:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 11:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+            case 12:
+                if(am_pm == 1){
+                    //serial send(xx grados);
+                }else {
+                    //serial send(xx + yy grados);
+                }
+                break;
+        }
+    }
+
+
+
+
+
 }
 
